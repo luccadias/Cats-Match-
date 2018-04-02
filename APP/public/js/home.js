2d22c8f62dfbd9e69a4f5f30301f5e8f09625fae
Lucca Arie Dias <luccarie@br.ibm.com>
2018-04-02 13:37:24 -0300

    :cat:

--- APP/public/js/home.js
+++ APP/public/js/home.js
@@ -3,13 +3,33 @@ var auth = localStorage.getItem("catsmathObj")
 if (authLogin(auth)) {
     $(document).ready(function () {
         var objPessoa = (JSON.parse(auth))
-        $("body").append("<h1>" + objPessoa.username + "</h1>");
-        $("body").append("<p>" + objPessoa.email + "</p>");
-
+        $(".username").text(objPessoa.username)
         $("#logout").click(function () {
             localStorage.removeItem("catsmathObj");
             window.location.href = "/";
         })
+
+        $("#menuSand").click(function(){
+            $(this).css("display","none");
+            $(".menu-left").css("display","block");
+        })
+        // $(".fa-times").click(function(){
+            
+        //     $(".menu-left").css("display", "none")
+        // })
+        
+        $( "#closeMenu" ).click(function() {
+            $( ".menu-left" ).animate({
+              width: "toggle",
+              height: "toggle"
+            }, {
+              duration: 500,
+              complete: function() {
+                $("#menuSand").css("display","block");
+                $(".menu-left").css("display", "none");
+              }
+            });
+          });
     })
 } else {
     window.location.href = "/";
